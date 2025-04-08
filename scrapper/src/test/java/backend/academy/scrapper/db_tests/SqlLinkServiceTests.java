6f61f1a2e6c21b2514bc41148d9abccd35029d35
plaffyyy <k.zimin@innopolis.university>
2025-04-08 12:42:20 +0300

    feat: add tests for sql link service

---
+++ scrapper/src/test/java/backend/academy/scrapper/db_tests/SqlLinkServiceTests.java
@@ -0,0 +1,89 @@
+package backend.academy.scrapper.db_tests;
+
+import backend.academy.scrapper.database_config.DbConfigTest;
+import backend.academy.scrapper.entities.Chat;
+import backend.academy.scrapper.entities.Link;
+import backend.academy.scrapper.services.data.SqlLinkService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.TestPropertySource;
+import java.util.List;
+import java.util.Set;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@Slf4j
+@SpringBootTest
+@TestPropertySource(properties = {
+    "access-type=SQL",
+    "spring.jpa.hibernate.ddl-auto=none"
+})
+//я исключаю создание hibernate здесь, так как его контекст подтягивает
+//а в случае появления hibernate они создают свои бины и ждут их в классе в конструкторе
+public class SqlLinkServiceTests extends DbConfigTest {
+
+    @Autowired
+    public SqlLinkServiceTests(SqlLinkService sqlLinkService, JdbcTemplate jdbcTemplate) {
+        this.sqlLinkService = sqlLinkService;
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    private final SqlLinkService sqlLinkService;
+    private final JdbcTemplate jdbcTemplate;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @BeforeEach
+    void setupDatabase() {
+
+    }
+
+    @DisplayName("Проверка, что корректно добавляется и удаляется чат")
+    @Test
+    public void testCreateAndDeleteChat() {
+        sqlLinkService.createChatById(1L);
+
+        Integer count = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM chat WHERE id = ?", Integer.class, 1L);
+        assertNotNull(count);
+        assertEquals(1, count);
+
+        sqlLinkService.deleteChatById(1L);
+        count = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM chat WHERE id = ?", Integer.class, 1L);
+        assertNotNull(count);
+        assertEquals(0, count);
+    }
+
+    @DisplayName("Проверка, что корректно добавляется и удаляется ссылка, а также верно отображаются фильтры")
+    @Test
+    public void testAddGetAndRemoveLink() {
+
+        Chat chat = new Chat(1L);
+        Link link = new Link(1L, "https://github.com/plaffyyy/SpringMVCLearn",
+            null, List.of("first", "second"), null, chat);
+
+        sqlLinkService.createChatById(chat.id());
+        sqlLinkService.addLink(1L, link);
+
+        Set<Link> links = sqlLinkService.getLinksByChatId(chat.id());
+        assertNotNull(links);
+        assertEquals(1, links.size());
+        assertEquals(link.url(), links.iterator().next().url());
+        assertEquals(link.filters(), links.iterator().next().filters());
+
+        sqlLinkService.removeLinkByUrl(chat.id(), link.url());
+        links = sqlLinkService.getLinksByChatId(chat.id());
+        assertNotNull(links);
+        assertEquals(0, links.size());
+
+
+    }
+
+
+
+}
