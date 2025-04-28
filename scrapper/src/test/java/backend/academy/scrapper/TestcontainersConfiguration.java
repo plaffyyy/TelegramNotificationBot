3c11c73aacbd7f6fa5bba78e1d59cb1b7d151b4e
plaffyyy <k.zimin@innopolis.university>
2025-04-28 12:24:15 +0300

    feat: add comfortable tests structure

--- scrapper/src/test/java/backend/academy/scrapper/TestcontainersConfiguration.java
+++ scrapper/src/test/java/backend/academy/scrapper/TestcontainersConfiguration.java
@@ -1,40 +1,48 @@
-// package backend.academy.scrapper;
-//
-// import org.springframework.boot.devtools.restart.RestartScope;
-// import org.springframework.boot.test.context.TestConfiguration;
-// import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-// import org.springframework.context.annotation.Bean;
-// import org.testcontainers.containers.GenericContainer;
-// import org.testcontainers.containers.PostgreSQLContainer;
-// import org.testcontainers.kafka.KafkaContainer;
-// import org.testcontainers.utility.DockerImageName;
-//
-//// isolated from the "bot" module's containers!
-// @TestConfiguration(proxyBeanMethods = false)
-// class TestcontainersConfiguration {
-//
-//    @Bean
-//    @RestartScope
-//    @ServiceConnection(name = "redis")
-//    GenericContainer<?> redisContainer() {
-//        return new GenericContainer<>(DockerImageName.parse("redis:7-alpine")).withExposedPorts(6379);
-//    }
-//
-//    @Bean
-//    @RestartScope
-//    @ServiceConnection
-//    PostgreSQLContainer<?> postgresContainer() {
-//        return new PostgreSQLContainer<>("postgres:17-alpine")
-//                .withExposedPorts(5432)
-//                .withDatabaseName("local")
-//                .withUsername("postgres")
-//                .withPassword("test");
-//    }
-//
-//    @Bean
-//    @RestartScope
-//    @ServiceConnection
-//    KafkaContainer kafkaContainer() {
-//        return new KafkaContainer("apache/kafka-native:3.8.1").withExposedPorts(9092);
-//    }
-// }
+package backend.academy.scrapper;
+
+import org.springframework.boot.devtools.restart.RestartScope;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.kafka.KafkaContainer;
+import org.testcontainers.utility.DockerImageName;
+import liquibase.integration.spring.SpringLiquibase;
+import javax.sql.DataSource;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.test.context.SpringBootTest;
+// isolated from the "bot" module's containers!
+@TestConfiguration(proxyBeanMethods = false)
+@SpringBootTest
+@Testcontainers
+@ComponentScan("backend.academy.scrapper.services.data")
+class TestcontainersConfiguration {
+    
+
+    @Bean
+    @RestartScope //one container per test class
+    @ServiceConnection(name = "redis")
+    GenericContainer<?> redisContainer() {
+        return new GenericContainer<>(DockerImageName.parse("redis:7-alpine")).withExposedPorts(6379);
+    }
+
+    @Bean
+    @RestartScope
+    @ServiceConnection
+    KafkaContainer kafkaContainer() {
+        return new KafkaContainer("apache/kafka-native:3.8.1").withExposedPorts(9092);
+    }
+
+    // @Bean
+    // public SpringLiquibase liquibase(DataSource dataSource) {
+    //     SpringLiquibase liquibase = new SpringLiquibase();
+    //     liquibase.setChangeLog("classpath:migrations/changelog/master.xml");
+    //     liquibase.setDataSource(dataSource);
+    //     liquibase.setDefaultSchema("public");
+    //     return liquibase;
+    // }
+}
