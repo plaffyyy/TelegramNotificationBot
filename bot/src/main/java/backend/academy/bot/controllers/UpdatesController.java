30166f9a20bb12127773606e77cf1400afac6d6f
plaffyyy <k.zimin@innopolis.university>
2025-05-01 19:44:57 +0300

    feat: add updates via configuration for controller and kafka

--- bot/src/main/java/backend/academy/bot/controllers/UpdatesController.java
+++ bot/src/main/java/backend/academy/bot/controllers/UpdatesController.java
@@ -3,7 +3,10 @@ package backend.academy.bot.controllers;
 import backend.academy.bot.notifier.Notifier;
 import java.util.List;
 import java.util.Map;
+import backend.academy.bot.notifier.config.NotificationConfig;
+import backend.academy.bot.services.NotificationRedisCache;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,13 +14,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @AllArgsConstructor
 @RestController
 @RequestMapping("/updates")
 public class UpdatesController {
 
-    @Autowired
     private final Notifier notifier;
+    private final NotificationConfig notificationConfig;
+    private final NotificationRedisCache notificationRedisCache;
 
     @PostMapping
     public ResponseEntity<Void> postUpdate(@RequestBody Map<String, Object> request) {
@@ -30,7 +35,15 @@ public class UpdatesController {
                 .append(url)
                 .append("\n");
         message.append(description);
-        notifier.notifyUsers(ids, message.toString());
+
+
+        if (notificationConfig.mode().equals(NotificationConfig.NotificationMode.IMMEDIATE)) {
+            notifier.notifyUsers(ids, message.toString());
+        } else if (notificationConfig.mode().equals(NotificationConfig.NotificationMode.DAILY_DIGEST)){
+            ids.forEach(id -> notificationRedisCache.addNotification(id, message.toString()));
+        } else {
+            log.error("Error in configuration mode in application.yaml");
+        }
 
         return ResponseEntity.ok(null);
     }
--- bot/src/main/java/backend/academy/bot/controllers/kafka/LinkUpdateListener.java
+++ bot/src/main/java/backend/academy/bot/controllers/kafka/LinkUpdateListener.java
@@ -1,7 +1,10 @@
 package backend.academy.bot.controllers.kafka;
 
 import backend.academy.bot.dto.LinkUpdateRequest;
+import backend.academy.bot.notifier.NotificationScheduler;
 import backend.academy.bot.notifier.Notifier;
+import backend.academy.bot.notifier.config.NotificationConfig;
+import backend.academy.bot.services.NotificationRedisCache;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -17,6 +20,8 @@ import java.util.List;
 public class LinkUpdateListener {
 
     private final Notifier notifier;
+    private final NotificationConfig notificationConfig;
+    private final NotificationRedisCache notificationRedisCache;
 
     @KafkaListener(
         topics = "${spring.kafka.topic.updates}",
@@ -30,16 +35,16 @@ public class LinkUpdateListener {
         @Header(KafkaHeaders.OFFSET) List<Long> offsets
     ) {
         log.info("Received batch of {} messages", requests.size());
-        
+
         for (int i = 0; i < requests.size(); i++) {
             LinkUpdateRequest request = requests.get(i);
             int partition = partitions.get(i);
             long offset = offsets.get(i);
-            
+
             try {
-                log.info("Processing message: partition={}, offset={}, request={}", 
+                log.info("Processing message: partition={}, offset={}, request={}",
                     partition, offset, request);
-                
+
                 List<Long> ids = request.ids() == null ? List.of() : request.ids();
                 String url = request.url();
                 String description = request.description();
@@ -49,13 +54,19 @@ public class LinkUpdateListener {
                     .append(url)
                     .append("\n");
                 message.append(description);
-                
-                notifier.notifyUsers(ids, message.toString());
-                
-                log.info("Successfully processed message: partition={}, offset={}", 
+
+                if (notificationConfig.mode().equals(NotificationConfig.NotificationMode.IMMEDIATE)) {
+                    notifier.notifyUsers(ids, message.toString());
+                } else if (notificationConfig.mode().equals(NotificationConfig.NotificationMode.DAILY_DIGEST)){
+                    ids.forEach(id -> notificationRedisCache.addNotification(id, message.toString()));
+                } else {
+                    log.error("Error in configuration mode in application.yaml");
+                }
+
+                log.info("Successfully processed message: partition={}, offset={}",
                     partition, offset);
             } catch (Exception e) {
-                log.error("Error processing message: partition={}, offset={}, error={}", 
+                log.error("Error processing message: partition={}, offset={}, error={}",
                     partition, offset, e.getMessage(), e);
                 // Individual message failure doesn't stop batch processing
             }
