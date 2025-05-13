795b2b630e8fc08cc5ecd3e0723be1b8d79c5059
plaffyyy <k.zimin@innopolis.university>
2025-05-13 23:43:10 +0300

    feat: realize all architecture patterns for track and untrack commands

--- scrapper/src/main/java/backend/academy/scrapper/controllers/ChatController.java
+++ scrapper/src/main/java/backend/academy/scrapper/controllers/ChatController.java
@@ -1,6 +1,10 @@
 package backend.academy.scrapper.controllers;
 
 import backend.academy.scrapper.services.data.LinkService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -8,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @Slf4j
 @AllArgsConstructor
@@ -18,12 +23,23 @@ public final class ChatController {
     private final LinkService linkService;
 
     @PostMapping("/{id}")
-    public void register(@PathVariable Long id) {
-        linkService.createChatById(id);
+    @RateLimiter(name = "apiRateLimiter")
+    @TimeLimiter(name = "httpTimeout")
+    @Retry(name = "httpRetry")
+    @CircuitBreaker(name = "httpCB")
+    public Mono<Void> register(@PathVariable Long id) {
+        return Mono.fromRunnable(() -> {
+            linkService.createChatById(id);
+        });
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id) {
-        linkService.deleteChatById(id);
-    }
+    @RateLimiter(name = "apiRateLimiter")
+    @TimeLimiter(name = "httpTimeout")
+    @Retry(name = "httpRetry")
+    @CircuitBreaker(name = "httpCB")
+    public Mono<Void> delete(@PathVariable Long id) {
+        return Mono.fromRunnable(() -> {
+            linkService.deleteChatById(id);
+        });    }
 }
